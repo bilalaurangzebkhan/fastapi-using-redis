827652e2fe51deee36c37496eb30d7aae26381f5
Bilal Aurangzeb Khan <78090865+bilalaurangzebkhan@users.noreply.github.com>
2024-09-06 15:59:38 +0500

    commit local build proj

---
+++ src/app/api/[[...route]]/route copy.ts
@@ -0,0 +1,71 @@
+import { Redis } from "@upstash/redis";
+import { Hono } from "hono";
+import { env } from "hono/adapter";
+import { handle } from "hono/vercel";
+
+export const runtime = "edge"
+
+const app = new Hono().basePath('/api')
+
+type EnvConfig ={
+    UPSTASH_REDIS_REST_tOKEN: string
+    UPSTASH_RADIS_REST_URL: string
+}
+
+app.get('/search', async (c) => {
+    try {
+        const {UPSTASH_REDIS_REST_tOKEN, UPSTASH_RADIS_REST_URL} = env<EnvConfig>(c)
+
+        const start = performance.now()
+    
+        // --------------------------------------
+    
+        const redis = new Redis ({
+            token: UPSTASH_REDIS_REST_tOKEN,
+            url: UPSTASH_RADIS_REST_URL,
+        })
+    
+        const query = c.req.query("q")?.toLocaleUpperCase()
+    
+        if(!query) {
+            return c.json({ message: 'Invalid search query' }, { status: 400} )
+        }
+    
+        const res = []
+        const rank = await redis.zrank("terms", query)
+    
+        if(rank !== null && rank !== undefined) {
+            const temp = await redis.zrange<string[]>("terms", rank, rank + 100)
+    
+            for(const el of temp) {
+                if(!el.startsWith(query)) {
+                    break
+                }
+    
+                if(el.endsWith('*')) {
+                    res.push(el.substring(0,  el.length - 1 ))
+                }
+            }
+        }
+        
+        // --------------------------------------
+        const end = performance.now()
+    
+        return c.json({
+            results: res,
+            duration: end - start
+        })
+
+    } catch (err) {
+        console.error(err)
+        return c.json(
+            { results: [], message: "Something went wrong" },
+            {
+                status: 500,
+            }
+        )
+    }
+})
+
+export const GET = handle(app)
+export default app as never
